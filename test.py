0337ffc3950c44bfde478e0e2bc45a1a8cbd9ee4
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-08 00:32:19 +0900

    delete : test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from pprint import pprint
-
-with open("commit_counts.pkl", "rb") as f:
-    sorted_commit_counts = pickle.load(f)
-
-
-for rank, (team, count) in enumerate(sorted_commit_counts, start=1):
-    pprint(rank, team, count)
